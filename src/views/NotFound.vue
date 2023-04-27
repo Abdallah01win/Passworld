93fdc6094e929587533369eab30b43a6b7d30167
Abdallah Bari <abdellah01win@gmail.com>
2023-04-27 13:15:24 +0100

    build not found page and redirection functionality

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,10 +1,51 @@
-<script setup>
+<script>
+import { ref, onMounted, computed } from 'vue';
+import { useRouter } from 'vue-router';
+
+export default {
+  setup() {
+    const router = useRouter();
+    const timer = ref(null);
+    const count = ref(5);
+
+    const startTimer = () => {
+      timer.value = setInterval(() => {
+        count.value--;
+        if (count.value === 0) {
+          clearInterval(timer.value);
+          router.push('/');
+        }
+      }, 1000);
+    };
+
+    onMounted(() => {
+      startTimer();
+    });
+
+    const handleClick = () => {
+      if (timer.value !== null) {
+        clearTimeout(timer.value);
+        router.push('/');
+      } else {
+        startTimer();
+      }
+    };
+
+    const buttonText = computed(() => {
+      return `Going Back Home in ${count.value}`;
+    });
+
+    return { handleClick, buttonText };
+  }
+};
 </script>
 
 <template>
-     <main class="flex items-center justify-between w-full">
-        <div class="text-center w-[45%] flex flex-col gap-y-4">
-            Not Found
+     <main class="flex items-center justify-center h-full">
+        <div class="flex flex-col items-center gap-y-8">
+            <img src="../assets/404.svg" alt="404" class="mb-6">
+            <p class="font-bold text-lg bg-white border-[3px] border-myBlack py-4 text-center w-full rounded-lg">Page Not Found... There's Nothing Here!</p>
+            <button @click="handleClick" class="bottomShadow py-2 px-4 font-bold w-fit bg-myGold-300 border-[3px] border-myBlack boxShadow rounded-lg active:translate-y-[4px] transition-all">{{ buttonText }}</button>
         </div>
      </main>
 </template>
