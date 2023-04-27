e23499c91c383f50b6878eeb67c02887a2dbb55f
Abdallah Bari <abdellah01win@gmail.com>
2023-04-27 12:42:41 +0100

    add not-found page and setup vue router

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,11 +1,14 @@
 <script setup>
+import { RouterLink } from "vue-router";
 import Logo from "./Logo.vue";
 import IconUsa from "./icons/IconUsa.vue";
 </script>
 <template>
   <nav class="flex items-center justify-between px-8">
     <div>
-      <Logo />
+      <RouterLink to="/">
+        <Logo />
+      </RouterLink>
     </div>
     <button
       class="bg-white border-[3px] p-[2px] border-myBlack boxShadow flex items-center right-radious"
---
+++ src/views/NotFound.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+
+<template>
+     <main class="flex items-center justify-between w-full">
+        <div class="text-center w-[45%] flex flex-col gap-y-4">
+            Not Found
+        </div>
+     </main>
+</template>
