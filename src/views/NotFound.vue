b31e913c0c6689f4ce93e0628a841bb3490863a4
Abdallah Bari <abdellah01win@gmail.com>
2023-08-06 12:35:59 +0100

    404 page responsivness

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -48,14 +48,12 @@ export default {
   <main class="mesh h-screen relative clip">
     <Navigation />
     <section class="w-full absolute top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%]">
-
-      <div class="py-12 xl:max-w-[1024px] mx-auto text-center">
-        <div class="capitalize font-bold text-6xl leading-tight mb-8">404 Page not Found</div>
-        <button @click="handleClick"
-          class="border border-myBlack rounded-xl py-3 px-8 cursor-pointer">{{
-            buttonText }}</button>
+      <div
+        class="max-w-[300px] xs:max-w-[365px] sm:max-w-[520px] md:max-w-[680px] lg:max-w-[940px] xl:max-w-[1024px] mx-auto py-12 text-center">
+        <div class="capitalize font-bold text-4xl sm:text-5xl md:text-6xl leading-tight mb-8">404 Page not Found</div>
+        <button @click="handleClick" class="border border-myBlack rounded-xl py-3 px-8 cursor-pointer">{{
+          buttonText }}</button>
       </div>
-
     </section>
   </main>
 </template>
