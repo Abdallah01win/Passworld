7cfa0bf7bde770f38d5486e79ff7dd89a7306e8d
Abdallah Bari <abdellah01win@gmail.com>
2023-12-24 12:37:37 +0100

    Fix index formatting and add robots and headers files

--- src/App.vue
+++ src/App.vue
@@ -21,16 +21,16 @@ import { RouterView } from "vue-router";
   background-color: #ffffff;
   background-repeat: no-repeat;
   background-image: radial-gradient(at 38% 41%,
-      hsla(166, 86%, 50%, 1) 0,
+      #12edba 0,
       hsla(166, 86%, 50%, 0) 50%),
     radial-gradient(at 92% 78%,
-      hsla(73, 94%, 51%, 1) 0,
+      #c5f80d 0,
       hsla(73, 94%, 51%, 0) 50%),
     radial-gradient(at 96% 90%,
-      hsla(93, 87%, 69%, 1) 0,
+      #a9f56b 0,
       hsla(93, 87%, 69%, 0) 50%),
     radial-gradient(at 53% 97%,
-      hsla(197, 93%, 68%, 1) 0,
+      #62cef9 0,
       hsla(197, 93%, 68%, 0) 50%);
 }
 </style>
